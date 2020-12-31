2aa304aa5cb5dcf92f47d3451a9a466cf9cab165
leeminkan <16520950@gm.uit.edu.vn>
2020-12-31 18:53:04 +0700

    implement apex realtime chart for comparision page

---
+++ src/pages/AllCharts/custom-chart/double-realtime-chart.js
@@ -0,0 +1,240 @@
+import React, { Component } from 'react';
+import ReactApexChart from 'react-apexcharts';
+import ApexCharts from "apexcharts";
+
+// Redux
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+// actions
+import { getChartData, updateStateChartData } from "../../../store/actions";
+import * as util from '../../../helpers/util';
+
+class DoubleRealtimeChart extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isMounted: false,
+            series: [{
+                data: []
+            }],
+            options: {
+                chart: {
+                    id: `realtime-${props.waspmote_id}-${props.sensor_key}`,
+                    height: 350,
+                    type: 'line',
+                    animations: {
+                        enabled: true,
+                        easing: 'linear',
+                        dynamicAnimation: {
+                            speed: 1000
+                        }
+                    },
+                    toolbar: {
+                        show: false
+                    },
+                    zoom: {
+                        enabled: false
+                    }
+                },
+                dataLabels: {
+                    enabled: false
+                },
+                stroke: {
+                    curve: 'smooth',
+                    width: 1
+                },
+                title: {
+                    text: props.sensor.name,
+                    align: 'center',
+                    style: {
+                      fontSize:  '14px',
+                      fontWeight:  'bold',
+                      fontFamily:  undefined,
+                      color:  '#263238'
+                    },
+                },
+                markers: {
+                    size: 0
+                },
+                xaxis: {
+                    type: 'datetime',
+                    // range: XAXISRANGE,
+                },
+                yaxis: {
+                    min: util.getOrFail(() => (props.sensor.chart_options.comparision_page.yaxis.min)) || util.getOrFail(() => (props.sensor.chart_options.general.yaxis.min)),
+                    max: util.getOrFail(() => (props.sensor.chart_options.comparision_page.yaxis.max)) || util.getOrFail(() => (props.sensor.chart_options.general.yaxis.max)),
+                    labels: {
+                      formatter: function (value) {
+                        return parseFloat(value).toFixed(2);
+                      }
+                    },
+                    title: {
+                      text: `${props.sensor.name} (${props.sensor.unit})`,
+                      rotate: 90,
+                      offsetX: 0,
+                      offsetY: 0,
+                      style: {
+                          color: undefined,
+                          fontSize: '12px',
+                          fontFamily: 'Helvetica, Arial, sans-serif',
+                          fontWeight: 600,
+                          cssClass: 'apexcharts-yaxis-title',
+                      },
+                    },
+                },
+                legend: {
+                    show: false
+                },
+                ...(util.getOrFail(() => (props.sensor.chart_options.comparision_page.options)) || util.getOrFail(() => (props.sensor.chart_options.general.options))),
+            },
+            series2: [{
+                data: []
+            }],
+            options2: {
+                chart: {
+                    id: `realtime-${props.waspmote_id}-${props.sensor_key}-2`,
+                    height: 350,
+                    type: 'line',
+                    animations: {
+                        enabled: true,
+                        easing: 'linear',
+                        dynamicAnimation: {
+                            speed: 1000
+                        }
+                    },
+                    toolbar: {
+                        show: false
+                    },
+                    zoom: {
+                        enabled: false
+                    }
+                },
+                dataLabels: {
+                    enabled: false
+                },
+                stroke: {
+                    curve: 'smooth',
+                    width: 1
+                },
+                title: {
+                    text: 'Time Get Sample',
+                    align: 'center',
+                    style: {
+                      fontSize:  '14px',
+                      fontWeight:  'bold',
+                      fontFamily:  undefined,
+                      color:  '#263238'
+                    },
+                },
+                markers: {
+                    size: 0
+                },
+                xaxis: {
+                    type: 'datetime',
+                    // range: XAXISRANGE,
+                },
+                yaxis: {
+                    labels: {
+                      formatter: function (value) {
+                        return parseFloat(value).toFixed(2);
+                      }
+                    },
+                },
+                legend: {
+                    show: false
+                },
+            },
+        };
+    }
+
+    componentDidMount() {
+        this.props.getChartData(this.props.history, this.props.waspmote_id, this.props.sensor_key, this.props.limit, this.props.sort, this.props.algorithm_parameter_id);
+        
+        let intervalId = window.setInterval(() => {
+            this.props.getChartData(this.props.history, this.props.waspmote_id, this.props.sensor_key, this.props.limit, this.props.sort, this.props.algorithm_parameter_id);
+        }, 5000);
+
+        this.setState({ 
+            intervalId: intervalId,
+            isMounted: true 
+        });
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.state.intervalId);
+        
+        const cloneData = Object.assign({}, this.props.data);
+
+        if (typeof cloneData[this.props.waspmote_id] !== 'undefined' 
+        && 
+        typeof cloneData[this.props.waspmote_id][this.props.sensor_key] !== 'undefined') {
+            cloneData[this.props.waspmote_id][this.props.sensor_key] = [];
+        }
+
+        this.props.updateStateChartData({
+          data: {
+              ...cloneData
+          }
+        });
+    }
+
+    render() {
+        if (typeof this.props.data[this.props.waspmote_id] !== 'undefined' 
+        && 
+        typeof this.props.data[this.props.waspmote_id][this.props.sensor_key] !== 'undefined') {
+            let chartData = this.props.data[this.props.waspmote_id][this.props.sensor_key];
+            let newData = chartData.map(element => ({
+                y: element.value,
+                x: element.created_at
+            }));
+
+            let newData2 = chartData.map(element => ({
+                y: element.time_get_sample,
+                x: element.created_at
+            }));
+            
+            if (this.props.reverse) {
+                newData = newData.reverse();
+                newData2 = newData2.reverse();
+            }
+
+            if (this.state.isMounted) {
+                ApexCharts.exec(`realtime-${this.props.waspmote_id}-${this.props.sensor_key}`, 'updateSeries', [{
+                    data: newData
+                }]);
+                ApexCharts.exec(`realtime-${this.props.waspmote_id}-${this.props.sensor_key}-2`, 'updateSeries', [{
+                    data: newData2
+                }]);
+            }
+        }
+        
+        return (
+            <React.Fragment>
+                <div className="double-chart-wrapper">
+                    <div className="double-chart-child-wrapper">
+                        <ReactApexChart 
+                        options={this.state.options} 
+                        series={this.state.series} 
+                        type="line" 
+                        height={350} />
+                    </div>
+                    <div className="double-chart-child-wrapper">
+                        <ReactApexChart 
+                        options={this.state.options2} 
+                        series={this.state.series2} 
+                        type="line" 
+                        height={350} />
+                    </div>
+                </div>
+            </React.Fragment>
+        );
+    }
+}
+
+
+const mapStatetoProps = state => {
+    return state.ChartData;
+};
+  
+export default withRouter(connect(mapStatetoProps, { getChartData, updateStateChartData })(DoubleRealtimeChart));
